96e50bc7f8206d44d8f9443810ea85eee5ab81d0
Artem <63448341+Gilah-EnE@users.noreply.github.com>
2025-07-01 19:46:19 +0300

    Sector size selection
    Now it is possible to change the sector size, which is important in various media type analysis (old HDDs, new HDDs, optical media etc.)

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	qt.NewQApplication(os.Args)
 	window := qt.NewQMainWindow(nil)
-	window.SetMinimumSize(qt.NewQSize2(600, 144))
+	window.SetMinimumSize(qt.NewQSize2(600, 192))
 	window.Show()
 	widget := qt.NewQWidget2()
 
@@ -44,14 +44,32 @@ func main() {
 	resultsLayout.AddWidget2(resultsLabel.QWidget, 0, 0)
 	resultsLayout.AddWidget2(resultsField.QWidget, 0, 1)
 
+	// Sector size selection
+	sectorSizeSwitchLayout := qt.NewQHBoxLayout2()
+	standardSectorSizeRadio := qt.NewQRadioButton4("512 bytes (default)", widget)
+	standardSectorSizeRadio.SetChecked(true)
+	opticalSectorSizeRadio := qt.NewQRadioButton4("2048 bytes (optical media)", widget)
+	advancedFormatSectorSizeRadio := qt.NewQRadioButton4("4096 bytes (AF disks)", widget)
+	sectorSizeSwitchLayout.AddWidget(standardSectorSizeRadio.QWidget)
+	sectorSizeSwitchLayout.AddWidget(opticalSectorSizeRadio.QWidget)
+	sectorSizeSwitchLayout.AddWidget(advancedFormatSectorSizeRadio.QWidget)
+
 	// Hail Mary mode
 	hailMarySwitch := qt.NewQCheckBox4("Hail Mary mode (all patterns checked in all sectors - slow, horrible and painful)", widget)
 
 	// Start button
 	startButton := qt.NewQPushButton5("Run", widget)
 	startButton.OnClicked(func() {
+		var sectorSize int
+		if standardSectorSizeRadio.IsChecked() {
+			sectorSize = 512
+		} else if opticalSectorSizeRadio.IsChecked() {
+			sectorSize = 2048
+		} else if advancedFormatSectorSizeRadio.IsChecked() {
+			sectorSize = 4096
+		}
 		resultsField.SetText("")
-		toolDetectionResult := toolDetection(fileNameField.Text(), 512, hailMarySwitch.IsChecked())
+		toolDetectionResult := toolDetection(fileNameField.Text(), sectorSize, hailMarySwitch.IsChecked())
 		for suite, value := range toolDetectionResult {
 			if value > 0 {
 				resultsField.SetText(suite)
@@ -63,6 +81,7 @@ func main() {
 	globalLayout := qt.NewQVBoxLayout(widget)
 	globalLayout.AddLayout(filePickerLayout.QLayout)
 	globalLayout.AddLayout(resultsLayout.QLayout)
+	globalLayout.AddLayout(sectorSizeSwitchLayout.QLayout)
 	globalLayout.AddWidget(hailMarySwitch.QWidget)
 	globalLayout.AddWidget(startButton.QWidget)
 
